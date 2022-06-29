4b08f1ee969d430bc18d149ba1e89fc31f619cd6
pacheco2403 <103113450+pacheco2403@users.noreply.github.com>
2022-06-28 21:25:49 -0300

    Add files via upload

---
+++ exerc_7.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sla(int num){
+	int valor, cont, soma = 0;
+	for (cont>=1;cont<valor;cont++){
+	soma=cont+valor;
+	return 0;
+	}
+	int main (){
+		int valor;
+	printf("Escreva seu numero \n");
+	scanf("%d", &valor);
+	printf("%d", soma);
+	return 0;
+	}
+
+	
+
