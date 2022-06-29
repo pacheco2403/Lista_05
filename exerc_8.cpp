f2d3544178e2a0b8c5c920c78911a0161e2bc1bb
pacheco2403 <103113450+pacheco2403@users.noreply.github.com>
2022-06-28 21:51:30 -0300

    Add files via upload

---
+++ exerc_8.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+float p( int sexo , float altura){
+	float man, woman;
+	int n;
+	
+printf("1 - M  /  2 - W");
+scanf("%d", &n);
+}
+	int main {
+	if(sexo== 1){ 
+		h= (72.7 * altura) - 58;
+		printf("HOMEM: %.2f", h);
+}
+	else if (sexo == 2){ 
+		m= (62.1 * altura) - 44.7;
+		printf("MULHER: %.2f", m);
+
+}
+	return man;
+	return woman;
+}
